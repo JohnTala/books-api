1ee0bf607c5bd35ea8e2b551e110cd348f98b2bd
JohnTala <57272492+JohnTala@users.noreply.github.com>
2026-03-24 19:54:28 +0200

    add Swagger auto-generation for Books API with Render URL

--- swagger.js
+++ swagger.js
@@ -2,17 +2,17 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Contacts API',
-    description: 'API documentation for the Contacts project'
+    title: 'Books API',
+    description: 'API documentation for the Books project'
   },
-  host: 'cse341-project1-1-4omy.onrender.com', // Your Render URL
-  schemes: ['https'],                             // HTTPS for Render
+  host: 'books-api-189e.onrender.com', // Your Render URL
+  schemes: ['https'],                   // HTTPS for Render
   consumes: ['application/json'],
   produces: ['application/json']
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/contacts.js', './routes/index.js'];
+const endpointsFiles = ['./routes/bookRoutes.js', './routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
   .then(() => {
